90412acc7238bdad93fcd7390ea50c9ff9701efa
yuki819 <c01193119d@edu.teu.ac.jp>
2022-08-02 20:42:48 +0900

    x

--- app/Http/Controllers/Sample/IndexController.php
+++ app/Http/Controllers/Sample/IndexController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //aaaa
+    public function show(){
+        return "Hello";
+    }
+
+    public function showId($id){
+        return "Hello {$id}";
+    }
 }
