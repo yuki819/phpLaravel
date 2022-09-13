d250afb71d8cfa65f80bcee539794e2e6fd12c20
yuki819 <c01193119d@edu.teu.ac.jp>
2022-09-13 14:56:40 +0900

    ユニットテストのサンプルを実装

---
+++ tests/Unit/Services/TweetServiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use PHPUnit\Framework\TestCase;
+use App\Services\TweetService;
+use Mockery;
+
+class TweetServiceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     * @runInSeparateProcess
+     * @return void
+     */
+    public function test_check_own_tweet()
+    {
+        $tweetService = new TweetService();
+
+        $mock = Mockery::mock('alias:App\Models\Tweet');
+        
+        $mock->shouldReceive('where->first')->andReturn((object)[
+            'id' => 1,
+            'user_id' => 1
+        ]);
+
+        $result = $tweetService->checkOwnTweet(1, 1);
+        $this->assertTrue($result);
+
+        $result = $tweetService->checkOwnTweet(2, 1);
+        $this->assertFalse($result);
+    }
+}
